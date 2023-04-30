5804885503a25e1c01e1da804b3e5d329ec40971
vlifirenko <vladislav.lifirenko@gmail.com>
2023-04-30 18:58:14 +0300

    Folder structure

--- NeuralNetwork/Neuron.cs
+++ NeuralNetwork/Neuron.cs
@@ -3,20 +3,36 @@
 	public class Neuron
 	{
 		public List<double> Weights { get; }
+		public List<double> Inputs { get; }
 		public ENeuronType NeuronType { get; }
 		public double Output { get; private set; }
+		public double Delta { get; private set; }
 
 		public Neuron(int inputCount, ENeuronType type = ENeuronType.Normal)
 		{
 			NeuronType = type;
 			Weights = new List<double>();
+			Inputs = new List<double>();
+
+			InitWeightsRandomValue(inputCount);
+		}
+
+		private void InitWeightsRandomValue(int inputCount)
+		{
+			var random = new Random();
 
 			for (int i = 0; i < inputCount; i++)
-				Weights.Add(1);
+			{
+				Weights.Add(random.NextDouble());
+				Inputs.Add(0);
+			}
 		}
 
 		public double FeedForward(List<double> inputs)
 		{
+			for (int i = 0; i < inputs.Count; i++)
+				Inputs[i] = inputs[i];
+
 			var sum = 0.0;
 			for (int i = 0; i < inputs.Count; i++)
 				sum += inputs[i] * Weights[i];
@@ -31,11 +47,37 @@
 
 		public void SetWeights(params double[] weights)
 		{
+			// TODO: remove
 			for (int i = 0; i < weights.Length; i++)
 				Weights[i] = weights[i];
 		}
 
+		public void Learn(double error, double learningRate)
+		{
+			if (NeuronType == ENeuronType.Input)
+				return;
+
+			Delta = error * SigmoidDx(Output);
+
+			for (int i = 0; i < Weights.Count; i++)
+			{
+				var weight = Weights[i];
+				var input = Inputs[i];
+				var newWeight = weight - input * Delta * learningRate;
+
+				Weights[i] = newWeight;
+			}
+		}
+
 		private static double Sigmoid(double x) => 1.0 / (1.0 + Math.Pow(Math.E, -x));
+
+		private static double SigmoidDx(double x)
+		{
+			var sigmoid = Sigmoid(x);
+			var result = sigmoid / 1 - sigmoid;
+
+			return result;
+		}
 
 		public override string ToString() => Output.ToString();
 	}
