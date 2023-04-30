949907a4b3deb9a37350802334300e41c0585848
vlifirenko <vladislav.lifirenko@gmail.com>
2023-04-30 15:59:49 +0300

    Base structure on neural network (without learning)

---
+++ Layer.cs
@@ -0,0 +1,23 @@
+﻿namespace NeuralNetwork
+{
+	public class Layer
+	{
+		public List<Neuron> Neurons { get; private set; }
+		public int Count => Neurons?.Count ?? 0;
+
+		public Layer(List<Neuron> neurons, ENeuronType type = ENeuronType.Normal)
+		{
+			Neurons = neurons;
+		}
+
+		public List<double> GetSignals()
+		{
+			var result = new List<double>();
+
+			foreach (var neuron in Neurons)
+				result.Add(neuron.Output);
+
+			return result;
+		}
+	}
+}
--- Neuron.cs
+++ Neuron.cs
@@ -3,9 +3,7 @@
 	public class Neuron
 	{
 		public List<double> Weights { get; }
-
 		public ENeuronType NeuronType { get; }
-
 		public double Output { get; private set; }
 
 		public Neuron(int inputCount, ENeuronType type = ENeuronType.Normal)
@@ -25,6 +23,12 @@
 
 			Output = Sigmoid(sum);
 			return Output;
+		}
+
+		public void SetWeights(params double[] weights)
+		{
+			for (int i = 0; i < weights.Length; i++)
+				Weights[i] = weights[i];
 		}
 
 		private static double Sigmoid(double x) => 1.0 / (1.0 + Math.Pow(Math.E, -x));
